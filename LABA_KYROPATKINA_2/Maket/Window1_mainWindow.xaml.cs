954716038fac5804a54337008183d51533cbc0ba
MakerCsharp <61178402+MakerCsharp@users.noreply.github.com>
2020-09-29 00:08:13 +0300

    -Добавлена доп страница для тестирования

---
+++ LABA_KYROPATKINA_2/Maket/Window1_mainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LABA_KYROPATKINA_2.Maket
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1_mainWindow.xaml
+    /// </summary>
+    public partial class Window1_mainWindow : Window
+    {
+        public Window1_mainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
